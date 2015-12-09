91f6790a81d1bc34d976407eb89dee05c2413973
EShimel <edieshimel@gmail.com>
2015-12-09 18:28:29 -0500

    adds front end delete capabilities...recognize!

--- js/api.js
+++ js/api.js
@@ -1,129 +1,4 @@
-  'use strict'
-//if storwe API isn't undefined it will remain its original value
-/*var myJournalAPI = myJournalAPI || {};
-
-//    myJournalAPI.login = function
-// myJournalAPI is the object!
-myJournalAPI.server = 'http://localhost:3000';
-
-// This ajax functionality wraps around ajax calls for register, list, etc
-myJournalAPI.ajax = function(config, cb) {
-  $.ajax(config).done(function(data, textStatus, jqxhr) {
-    cb(null, data);
-    }).fail(function(jqxhr, status, error) {
-    cb({jqxher: jqxhr, status: status, error: error});
-  });
- };
-
-myJournalAPI.register = function register(credentials, callback) {
-
-  this.ajax({
-    method: 'POST',
-    // url: 'http://httpbin.org/post',
-    url: this.server + '/register',
-    contentType: 'application/json; charset=utf-8',
-    data: JSON.stringify(credentials),
-    dataType: 'json'
-  }, callback);
-};
-
-myJournalAPI.login = function login(credentials, callback) {
-  this.ajax({
-    method: 'POST',
-    // url: 'http://httpbin.org/post',
-    url: this.server + '/login',
-    contentType: 'application/json; charset=utf-8',
-    data: JSON.stringify(credentials),
-    dataType: 'json'
-  }, callback);
-};
-
-myJournalAPI.contributions = function contributions(token, callback) {
-  this.ajax({
-    method: 'POST',
-    // url: 'http://httpbin.org/post',
-    url: this.server + '/contributions',
-   contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({}),
-      dataType: 'json',
-    }, callback);
-
-
-};
-
-myJournalAPI.clues = function clues(token, callback) {
-   this.ajax({
-     method: 'GET',
-     url: myJournalAPI.server + '/clues',
-     headers: {
-       Authorization: 'Token token=' + token,
-     },
-     dataType: 'json'
-    }, callback);
- };*/
-
-  //THIS IS ALL NONE WORKING    uthenticated api actions
-
-
-  /*createContribution: function (token, callback) {
-    this.ajax({
-      method: 'POST',
-      url: this.serve + '/',
-      headers: {
-        Authorization: 'Token token=' + token
-      },
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({}),
-      dataType: 'json',
-    }, callback);
-  },
-
-  showGame: function (id, token, callback) {
-    this.ajax({
-      method: 'GET',
-      url: this.ttt + '/games/' + id,
-      headers: {
-        Authorization: 'Token token=' + token
-      },
-      dataType: 'json'
-    }, callback);
-  },
-
-myJournalAPI.contribution(token, function(error, topicData){
-         if(error){}
-         // else means that the login worked
-         else {
-           console.log(JSON.stringify(topicData, null, 4));
-           topicData.topics.forEach(function(topic) {
-             // <li>topic.title</li>
-             $(".post-content").append("#right" + topic.title + "</li>");
-           }); //end of forEach loop for Topic Data
-         } //end of if error check
-       }); // *** end list topic ***
-
-
-myJournalAPI.previous_story = function previous_story( id, callback) {
-  this.ajax({
-    method: 'GET',
-    // url: 'http://httpbin.org/post',
-    url: this.server + '/story.all/' + id,
-    contentType: 'application/json; charset=utf-8',
-    data: JSON.stringify({}),
-    dataType: 'json'
-  }, callback);
-};
-
-myJournalAPI.inprogress = function inprogress(id, callback) {
-  this.ajax({
-    method: 'POST',
-    // url: 'http://httpbin.org/post',
-    url: this.server + '/stories',
-    contentType: 'application/json; charset=utf-8',
-    data: JSON.stringify(credentials),
-    dataType: 'json'
-  }, callback);
-};
-*/
+'use strict';
 
 
 var myJournalAPI = {
@@ -140,8 +15,6 @@ var myJournalAPI = {
 
 
 
-
-
   register: function (credentials, callback) {
     this.ajax({
       method: 'POST',
@@ -153,6 +26,7 @@ var myJournalAPI = {
     console.log("success");
   },
 
+
   login: function (credentials, callback) {
     this.ajax({
       method: 'POST',
@@ -163,6 +37,7 @@ var myJournalAPI = {
     }, callback);
     console.log("success");
   },
+
 
   logout: function (token, id, callback) {
     this.ajax({
@@ -188,18 +63,7 @@ var myJournalAPI = {
     }, callback);
   },
 
-  list_quick_posts: function(token, list_quick_posts, callback){
-    this.ajax({
-      method: 'GET',
-      url: this.url + '/quick_posts',
-      headers: {
-        Authorization: 'Token token=' + token
-      },
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(list_quick_posts),
-      dataType: 'json'
-    }, callback);
-  },
+
 
   new_entry: function (token, new_entry, callback) {
     this.ajax({
@@ -212,6 +76,46 @@ var myJournalAPI = {
       data: JSON.stringify(new_entry),
       dataType: 'json'
 
+    }, callback);
+  },
+
+   change_entry: function (token, id, change_entry, callback) {
+    this.ajax({
+      method: 'PATCH',
+      url: this.url + '/entries/' + id,
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(change_entry),
+      dataType: 'json'
+
+    }, callback);
+  },
+
+ destroy_entry: function (token, entryid, callback) {
+    this.ajax({
+      method: 'DELETE',
+      url: this.url + '/entries/' + entryid,
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json',
+      dataType: 'json'
+    }, callback);
+    console.log("success");
+  },
+
+   list_quick_posts: function(token, list_quick_posts, callback){
+    this.ajax({
+      method: 'GET',
+      url: this.url + '/quick_posts',
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(list_quick_posts),
+      dataType: 'json'
     }, callback);
   },
 
@@ -229,17 +133,5 @@ var myJournalAPI = {
     }, callback);
   },
 
-  change_entry: function (token, entry_id, change_entry, callback) {
-    this.ajax({
-      method: 'PATCH',
-      url: this.url + '/entries' + id,
-      headers: {
-        Authorization: 'Token token=' + token
-      },
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(change_entry),
-      dataType: 'json'
 
-    }, callback);
-  },
 };
--- js/script.js
+++ js/script.js
@@ -110,8 +110,24 @@ $(document).ready(function() {
       });
     });
       //deleting entry function
+    $('#deleteone').on('submit', function(e) {
+        e.preventDefault();
 
-      //update entry function
+        var token = $('.token').val();
+        var entryid = $('#deleteone > input[name="entryid"]').val();
+        console.log(entryid);
+        myJournalAPI.destroy_entry(token, entryid, function(err, data) {
+          if (err) {
+            console.error(err);
+            // do something with the error
+            return;
+          } else {
+            console.log (data);
+            //$('#deleteone').each(function(){
+            //this.reset();
+          }
+        });
+      });
 
       //  Submitting new quick_post function
 
@@ -132,23 +148,8 @@ $(document).ready(function() {
         });
       });
 
-      // change the listed entry
+      // update entry
 
-      $('#change-entry').on('submit', function(e) {
-        e.preventDefault();
-        var entry_id = entryID();
-        var token = $('.token').val();
-        var change_entry = wrap('entry', form2object(this));
-        myJournalAPI.change_entry(token, entry_id, change_entry, function(err,
-          entryData) {
-          if (err) {
-            // do something with the error
-            return;
-          } else {
-            console.log(entryData);
-          }
-        });
-      });
 
 
 
@@ -158,184 +159,7 @@ $(document).ready(function() {
 
 
 
-/*var myJournalAPI = myJournalAPI || {};
-
-var myJournalAPP = myJournalAPP || {};
-var token;
-var id;
-
-$("document").ready(function(){
-
-//if storwe API isn't undefined it will remain its original value
-//
-
-  var form2object = function(form) {
-    var data = {};
-    $(form).find('input').each(function(index, element) {
-    var type = $(this).attr('type');
-      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
-      data[$(this).attr('name')] = $(this).val();
-      }
-    });
-    return data;
-  };
-
-  var wrap = function wrap(root, formData) {
-    var wrapper = {};
-    wrapper[root] = formData;
-    return wrapper;
-  };
-
-  var callback = function callback(error, data) {
-    if (error) {
-     console.error(error);
-      $('#result').val('status: ' + error.status + ', error: ' + error.error);
-    return;
-  }
-// $('#result').val(JSON.stringify(data, null, 4));
-    console.log(JSON.stringify(data, null, 4));
-  };
-
-
-//lets me know if registration works
-$("#register").on('submit',function(e) {
-    e.preventDefault();
-    var credentials = wrap('credentials', form2object(this));
-    myJournalAPI.register(credentials, function(error, data){
-      if (error){
-        console.log("registration failed");
-      }
-      else
-      {
-        myJournalAPI.user = data.user;
-        var token = data.user.token;
-      }
-    });
-  });
 
 
 
 
-$('#login').submit(function(e) {
-   var credentials = wrap('credentials', form2object(this));
-   e.preventDefault();
-   myJournalAPI.login(credentials, function(error, data){
-     if (error){
-       console.log("Login failed");
-      } else {
-       // save all of the current users data
-       myJournalAPI.user = data.user;
-       //save the current user's token so it can be called in following fns
-       var token = data.user.token;
-//$("#login").submit(function() {
-  //  var url = $(this).attr('href');
-   // $("#center").load("login.html + #center");
-    //myJournalAPI.quick_posts(token, function(error, quick_postData){
-        // if(error){}
-         // else means that the login worked
-         // else {
-           //console.log(JSON.stringify(quick_postData, null, 4));
-
-           /*quick_postData.quick_posts.forEach(function(quick_post) {
-             // <li>topic.title</li>
-             $(".post-content")
-             .append('<form data-quick_post-id="'+quick_post.id+'">' + quick_post.quick_post + "</form>");
-           });//('<form>"#clues" + clue.clue + </form>');
-           *///end of forEach loop for Topic Data
-         //} //end of if error check
-       //});
-     //NEED }
-    //NEED});
-//NEED});
-/*$("#entries").on('submit',function(e) {
-    e.preventDefault();
-    var newEntry = wrap('entry', form2object(this));
-      if (e){
-        console.error(e);
-      }
-      else
-      {
-        myJournalAPI.user = data.user;
-        var token = data.user.token;
-      }
-
-  }); */
-
-/*Put this one back first!!$('#entries').on('submit', function(e) {
-        e.preventDefault();
-        var token = $('.token').val();
-        var new_entry = wrap('entry', form2object(this));
-        myJournalAPI.new_entry(token, new_entry, function(err, entryData) {
-          if (err) {
-            // do something with the error
-            return;
-          } else {
-            $('entries').each(function(){
-            this.reset();
-          });
-            console.log(entryData);
-          }
-        });
-
-/*$("#contribution").submit(function(){
-    var url = $(this).attr('href');
-    $("#right").load("contribution.html + #center + #right");
-$('#previous_story').load(function(e){
-    var id = wrap('id', form2object(this));
-    e.preventDefault();
-      myJournalAPI.story(id, function(error, data){
-      if (error){
-        console.log("story failed");
-      }
-      else
-      {
-        myJournalAPI.user = data.user;
-        var token = data.user.token;
-      }
-    });
-  });
-});
-
-
-
-
-
-
-
-
-   /*/// *** end list topic ***
-
-/*$("#login").on('submit',function(e) {
-    var credentials = wrap('credentials', form2object(this));
-    var cd = function cd(error, data) {
-      if (error){
-        callback(error);
-      }
-      return;
-    };
-    callback(null, data);
-    $(',token').val(data.user.token);
-    $('.id').validate.user.id;
-    console.log(data.user.token);
-//});*/
-/*Stories
- The most recent completed story is available to read on the login/registration page
- Stories are complete when Outline changes
- The story in progress is visible only after a contribution is submitted
-
-Outines
-Outlines are provided from the database upon login
-Outlines change every ten Contributions
-
-Clues
-Users receive clues from the previous user upon login, unless you are the first user
-After they submit their contribution, users submit clues for the next user
-
-Contributions
-Users make a contribution and submit it upon login
-A contribution is appended to the story in progress after submission
-Stories are made up of ten contributions
-
-});
-});
-*/
